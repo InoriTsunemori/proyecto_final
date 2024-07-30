8597ce38ff85a313f4d9febf580184cde27a05f7
Nadine Poyato Manzano <nadine.pyto.mnzno@gmail.com>
2024-07-30 17:03:52 +0200

    nuevas funciones

---
+++ funciones.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pandas as pd
+import os 
+import io
+
+def create_csv_file(data):
+
+    # Define el directorio y el nombre del archivo
+    DIRECTORIO_CSV = r'C:\Users\BORJA\OneDrive\Escritorio\HAB2\Proyecto_final\respuestas'
+    NOMBRE_ARCHIVO = "respuestas_cuestionario.csv"
+    
+    if not os.path.exists(DIRECTORIO_CSV):
+        os.makedirs(DIRECTORIO_CSV)
+    
+
+    ruta_csv = os.path.join(DIRECTORIO_CSV, NOMBRE_ARCHIVO)
+    
+
+    if os.path.exists(ruta_csv):
+        
+        data.to_csv(ruta_csv, mode='a', header=False, index=False)
+    else:
+        
+        data.to_csv(ruta_csv, mode='w', header=True, index=False)
+
+
+def reverse(options,dicc):
+
+    options_reverse = {v: k for k, v in options.items()}
+    response = {key: options_reverse[value] for key, value in dicc.items()}
+
+    return response
+
+
+
+
+
+
+
+if __name__=='__main__':
+    create_csv_file()
