80b4d4a4b344249c81a72ab0472f423739bb54ef
InoriTsunemori <164060394+InoriTsunemori@users.noreply.github.com>
2024-08-11 00:24:52 +0200

    Update app_cuestionario.py

--- app_cuestionario.py
+++ app_cuestionario.py
@@ -365,6 +365,16 @@ def demog():
     if st.button('Volver al Vocabulary Check List', on_click=lambda: navigate_page('Vocabulary Check List')):
         navigate_page('Vocabulary Check List')
 
+     if st..button('Enviar cuestionario completo'):
+            if 'response1' in st.session_state and 'response2' in st.session_state and 'response3' in st.session_state and 'response4' in st.session_state:
+                save_response(response())
+                st.success('Respuestas enviadas con éxito.')
+                st.session_state.questionnaire_submitted = True
+    
+            else:
+                st.error('Por favor, complete las secciones anteriores antes de enviar.')
+
+
 def principal():
 
     st.title(":blue[Proyecto Final: Cuestionario Dass]")
