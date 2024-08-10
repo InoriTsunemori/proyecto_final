f705f048b39acbc5f330bf995e9169cb02c8770a
Nadine Poyato Manzano <nadine.pyto.mnzno@gmail.com>
2024-08-10 13:24:04 +0200

    firebase

--- firebase_config.py
+++ firebase_config.py
@@ -1,7 +1,24 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import streamlit as st
 
-cred = credentials.Certificate('clave_json_firebase\proyectopinaldass-firebase-adminsdk-q0mci-b5f55f4c18.json')
+# cred = credentials.Certificate('clave_json_firebase\proyectopinaldass-firebase-adminsdk-q0mci-b5f55f4c18.json')
+# firebase_admin.initialize_app(cred)
+# db = firestore.client()
+
+cred = credentials.Certificate({
+    "type": st.secrets["firebase"]["type"],
+    "project_id": st.secrets["firebase"]["project_id"],
+    "private_key_id": st.secrets["firebase"]["private_key_id"],
+    "private_key": st.secrets["firebase"]["private_key"].replace("\\n", "\n"),
+    "client_email": st.secrets["firebase"]["client_email"],
+    "client_id": st.secrets["firebase"]["client_id"],
+    "auth_uri": st.secrets["firebase"]["auth_uri"],
+    "token_uri": st.secrets["firebase"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"]
+})
+
 firebase_admin.initialize_app(cred)
-db = firestore.client()
 
+db = firestore.client()
