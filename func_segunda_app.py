a040c23a4dd4dfa2639211496c3e3c5542f108a6
Nadine Poyato Manzano <nadine.pyto.mnzno@gmail.com>
2024-08-12 14:17:42 +0200

    pruebas

--- func_segunda_app.py
+++ func_segunda_app.py
@@ -2,10 +2,6 @@ import streamlit as st
 from google.cloud import firestore
 from google.cloud.firestore import Client
 import pandas as pd
-import plotly.express as px
-import matplotlib.pyplot as plt
-import seaborn as sns
-
 
 def get_db():
     key_json = st.secrets["firebase_key"]
@@ -13,155 +9,192 @@ def get_db():
     return db
 
 def importar_datos_firebase():
+
     db=get_db()
+    documents = db.collection('responses').stream()
+    data = []
 
-    collection_ref = db.collection("responses")
-    docs = collection_ref.stream()
-    
-    datos = []
-    
-    for doc in docs:
-        st.write(f"Documento principal encontrado: {doc.id}")
+    for doc in documents:
+        data.append(doc.to_dict())
 
-        subcollection_ref = collection_ref.document(doc.id).collections()
+    return data
+
+def mostrar_datos():
+    datos = importar_datos_firebase()
+
+    if datos: 
+        df=pd.DataFrame(datos)
+        st.write('Respuestas de los usuarios.')
+        st.dataframe(df)
+
+    else: 
+        st.write('No se han encontrado los datos.')
+
+
+
+
+
+
+
+
+
+
+
+# def get_db():
+#     key_json = st.secrets["firebase_key"]
+#     db = firestore.Client.from_service_account_info(key_json)
+#     return db
+
+# def importar_datos_firebase():
+#     db=get_db()
+
+#     collection_ref = db.collection("responses")
+#     docs = collection_ref.stream()
+    
+#     datos = []
+    
+#     for doc in docs:
+#         st.write(f"Documento principal encontrado: {doc.id}")
+
+#         subcollection_ref = collection_ref.document(doc.id).collections()
         
-        for subcollection in subcollection_ref:
-            sub_docs = subcollection.stream()
-            for sub_doc in sub_docs:
-                st.write(f"Documento en subcolección encontrado: {sub_doc.id}")
-                datos.append(sub_doc.to_dict())
+#         for subcollection in subcollection_ref:
+#             sub_docs = subcollection.stream()
+#             for sub_doc in sub_docs:
+#                 st.write(f"Documento en subcolección encontrado: {sub_doc.id}")
+#                 datos.append(sub_doc.to_dict())
     
-    st.write(f"Total de documentos encontrados en subcolecciones: {len(datos)}")
+#     st.write(f"Total de documentos encontrados en subcolecciones: {len(datos)}")
 
-    return datos
+#     return datos
 
 
-def func_identify_false_words(df, false_words):
+# def func_identify_false_words(df, false_words):
            
-    df['False_Words_Count'] = df[false_words].apply(lambda col : (col == 1).sum(), axis=1)
-    df['False_Word'] = df['False_Words_Count'].apply(lambda x: 1 if x > 0 else 0)
+#     df['False_Words_Count'] = df[false_words].apply(lambda col : (col == 1).sum(), axis=1)
+#     df['False_Word'] = df['False_Words_Count'].apply(lambda x: 1 if x > 0 else 0)
     
-    return df
+#     return df
 
-def plot_dmg_1 (df):
-    fig1 = px.scatter(df, x='gender', y='orientation', title='Relación género con orientación.')
-    return fig1
+# def plot_dmg_1 (df):
+#     fig1 = px.scatter(df, x='gender', y='orientation', title='Relación género con orientación.')
+#     return fig1
 
-def plot_dmg_2 (df):
-    fig2 = px.histogram(df, x='age', title='Distribución por edad.')
-    return fig2    
+# def plot_dmg_2 (df):
+#     fig2 = px.histogram(df, x='age', title='Distribución por edad.')
+#     return fig2    
 
-def plot_dmg_3 (df):
-    fig3 = px.scatter(df, x='orientation', y= 'major', title= 'Relación entre religión y estudios')
-    return fig3
+# def plot_dmg_3 (df):
+#     fig3 = px.scatter(df, x='orientation', y= 'major', title= 'Relación entre religión y estudios')
+#     return fig3
 
-# Gráfico palabras falsas
-def barplot_vcl(df):
-    false_words = ['VCL6', 'VCL9', 'VCL12']
-    df = func_identify_false_words(df, false_words)
+# # Gráfico palabras falsas
+# def barplot_vcl(df):
+#     false_words = ['VCL6', 'VCL9', 'VCL12']
+#     df = func_identify_false_words(df, false_words)
     
-    # Contar cuántas veces las palabras falsas han sido marcadas como 1
-    false_word_counts = df[false_words].apply(lambda col: (col == 1).sum())
+#     # Contar cuántas veces las palabras falsas han sido marcadas como 1
+#     false_word_counts = df[false_words].apply(lambda col: (col == 1).sum())
     
-    # Crear el gráfico de barras
-    fig = px.bar(
-        x = false_word_counts.index,
-        y = false_word_counts.values,
-        labels = {'x': 'Palabras Falsas', 'y': 'Cantidad de Veces Marcadas'},
-        title = 'Frecuencia de Palabras Falsas Marcadas', 
-    )
-    return fig
+#     # Crear el gráfico de barras
+#     fig = px.bar(
+#         x = false_word_counts.index,
+#         y = false_word_counts.values,
+#         labels = {'x': 'Palabras Falsas', 'y': 'Cantidad de Veces Marcadas'},
+#         title = 'Frecuencia de Palabras Falsas Marcadas', 
+#     )
+#     return fig
 
-#######################################################################################################
+# #######################################################################################################
 
-# Función para encontrar inconsistencias entre edad y educación
-def func_find_age_education_inconsistencies(df):
-    df = df.copy()
-    inconsistencies = df[(df['age'] < 21) & (df['education'] > 2)]
-    df['Inconsistency_Age_Education'] = 0
-    df.loc[inconsistencies.index, 'Inconsistency_Age_Education'] = 1
-    inconsistency_counts = df[df['Inconsistency_Age_Education'] == 1]['age'].value_counts().sort_index()
-    return df, inconsistencies, inconsistency_counts
+# # Función para encontrar inconsistencias entre edad y educación
+# def func_find_age_education_inconsistencies(df):
+#     df = df.copy()
+#     inconsistencies = df[(df['age'] < 21) & (df['education'] > 2)]
+#     df['Inconsistency_Age_Education'] = 0
+#     df.loc[inconsistencies.index, 'Inconsistency_Age_Education'] = 1
+#     inconsistency_counts = df[df['Inconsistency_Age_Education'] == 1]['age'].value_counts().sort_index()
+#     return df, inconsistencies, inconsistency_counts
 
-# Gráfico inconsistencias edad y educación
-def plot_inconsistencies_by_age(inconsistency_counts):
-    inconsistency_df = pd.DataFrame({
-        'Edad': inconsistency_counts.index,
-        'Cantidad de Inconsistencias': inconsistency_counts.values
-    })
-    fig = px.bar(
-        inconsistency_df,
-        x = 'Edad',
-        y = 'Cantidad de Inconsistencias',
-        title = 'Frecuencia de Inconsistencias entre edad y educación', 
-        labels = {'x': 'Edad', 'y': 'Cantidad de Inconsistencias'}
-    )
-    st.plotly_chart(fig)
+# # Gráfico inconsistencias edad y educación
+# def plot_inconsistencies_by_age(inconsistency_counts):
+#     inconsistency_df = pd.DataFrame({
+#         'Edad': inconsistency_counts.index,
+#         'Cantidad de Inconsistencias': inconsistency_counts.values
+#     })
+#     fig = px.bar(
+#         inconsistency_df,
+#         x = 'Edad',
+#         y = 'Cantidad de Inconsistencias',
+#         title = 'Frecuencia de Inconsistencias entre edad y educación', 
+#         labels = {'x': 'Edad', 'y': 'Cantidad de Inconsistencias'}
+#     )
+#     st.plotly_chart(fig)
     
-#######################################################################################################
+# #######################################################################################################
 
-# Function to display correlation matrix
-def func_plot_correlation_matrix(df, columns, figsize = (32, 28), cmap = 'coolwarm', annot = True, fmt = '.2f', linewidths = 0.5, center = 0):
+# # Function to display correlation matrix
+# def func_plot_correlation_matrix(df, columns, figsize = (32, 28), cmap = 'coolwarm', annot = True, fmt = '.2f', linewidths = 0.5, center = 0):
 
-    plt.figure(figsize=figsize)
-    correlation_matrix = df[columns].corr()
-    sns.heatmap(correlation_matrix, cmap=cmap, annot=annot, fmt=fmt, linewidths=linewidths, center=center, annot_kws={'size':9})
-    plt.title('Matriz de correlación', fontsize=20)
-    plt.xticks(fontsize=10, rotation=90)
-    plt.yticks(fontsize=10, rotation=0)
-    st.pyplot(plt.gcf())
+#     plt.figure(figsize=figsize)
+#     correlation_matrix = df[columns].corr()
+#     sns.heatmap(correlation_matrix, cmap=cmap, annot=annot, fmt=fmt, linewidths=linewidths, center=center, annot_kws={'size':9})
+#     plt.title('Matriz de correlación', fontsize=20)
+#     plt.xticks(fontsize=10, rotation=90)
+#     plt.yticks(fontsize=10, rotation=0)
+#     st.pyplot(plt.gcf())
     
-    return correlation_matrix
+#     return correlation_matrix
 
  
-# Function to find related pairs
-def func_find_related_pairs(correlation_matrix, columns, threshold = 0.6):
-    pairs = []
-    for i in range(len(columns)):
-        for j in range(i + 1, len(columns)):
-            if abs(correlation_matrix.iloc[i, j]) >= threshold:
-                pairs.append((columns[i], columns[j]))
-    return pairs
+# # Function to find related pairs
+# def func_find_related_pairs(correlation_matrix, columns, threshold = 0.6):
+#     pairs = []
+#     for i in range(len(columns)):
+#         for j in range(i + 1, len(columns)):
+#             if abs(correlation_matrix.iloc[i, j]) >= threshold:
+#                 pairs.append((columns[i], columns[j]))
+#     return pairs
 
-# Function to detect inconsistencies
-def func_detect_inconsistencies(df, related_pairs):
-    incoherencies_ids = []
-    inconsistencies = []
-    for (q1, q2) in related_pairs:
-        contradictions = df[((df[q1] == 0) & (df[q2] == 3)) | ((df[q1] == 3) & (df[q2] == 0))]
-        if not contradictions.empty:
-            incoherencies_ids.extend(contradictions.index)
-            inconsistencies.append({'question1': q1, 'question2': q2, 'contradictions': contradictions})
-    df['Inconsistency_Questions'] = 0
-    df.loc[incoherencies_ids, 'Inconsistency_Questions'] = 1
-    df['Inconsistency_Questions_Count'] = pd.Series(incoherencies_ids).value_counts().reindex(df.index, fill_value = 0)
-    return df, inconsistencies
+# # Function to detect inconsistencies
+# def func_detect_inconsistencies(df, related_pairs):
+#     incoherencies_ids = []
+#     inconsistencies = []
+#     for (q1, q2) in related_pairs:
+#         contradictions = df[((df[q1] == 0) & (df[q2] == 3)) | ((df[q1] == 3) & (df[q2] == 0))]
+#         if not contradictions.empty:
+#             incoherencies_ids.extend(contradictions.index)
+#             inconsistencies.append({'question1': q1, 'question2': q2, 'contradictions': contradictions})
+#     df['Inconsistency_Questions'] = 0
+#     df.loc[incoherencies_ids, 'Inconsistency_Questions'] = 1
+#     df['Inconsistency_Questions_Count'] = pd.Series(incoherencies_ids).value_counts().reindex(df.index, fill_value = 0)
+#     return df, inconsistencies
 
-def plot_inconsistencies_by_related_pairs(inconsistencies):
-    data = []
-    for inconsistency in inconsistencies:
-        q1 = inconsistency['question1']
-        q2 = inconsistency['question2']
-        count = len(inconsistency['contradictions'])
-        data.append({'Pares': f'{q1} & {q2}', 'Cantidad de Inconsistencias': count})
+# def plot_inconsistencies_by_related_pairs(inconsistencies):
+#     data = []
+#     for inconsistency in inconsistencies:
+#         q1 = inconsistency['question1']
+#         q2 = inconsistency['question2']
+#         count = len(inconsistency['contradictions'])
+#         data.append({'Pares': f'{q1} & {q2}', 'Cantidad de Inconsistencias': count})
     
-    inconsistency_df = pd.DataFrame(data)
+#     inconsistency_df = pd.DataFrame(data)
 
-    if not inconsistency_df.empty:
-        fig = px.bar(
-            inconsistency_df,
-            x = 'Pares',
-            y = 'Cantidad de Inconsistencias',
-            title = 'Cantidad de Inconsistencias por Pares Relacionados',
-            labels = {'Pares': 'Pares de Preguntas', 'Cantidad de Inconsistencias': 'Cantidad de Inconsistencias'},
-            text = 'Cantidad de Inconsistencias'
-        )
-        fig.update_layout(
-            xaxis_title = 'Pares de Preguntas',
-            yaxis_title = 'Cantidad de Inconsistencias',
-            xaxis_tickangle = -45
-        )
-        fig.update_traces(texttemplate = '%{text}', textposition = 'outside')
-        st.plotly_chart(fig)
-    else:
-        st.write("No inconsistencies to display.")
+#     if not inconsistency_df.empty:
+#         fig = px.bar(
+#             inconsistency_df,
+#             x = 'Pares',
+#             y = 'Cantidad de Inconsistencias',
+#             title = 'Cantidad de Inconsistencias por Pares Relacionados',
+#             labels = {'Pares': 'Pares de Preguntas', 'Cantidad de Inconsistencias': 'Cantidad de Inconsistencias'},
+#             text = 'Cantidad de Inconsistencias'
+#         )
+#         fig.update_layout(
+#             xaxis_title = 'Pares de Preguntas',
+#             yaxis_title = 'Cantidad de Inconsistencias',
+#             xaxis_tickangle = -45
+#         )
+#         fig.update_traces(texttemplate = '%{text}', textposition = 'outside')
+#         st.plotly_chart(fig)
+#     else:
+#         st.write("No inconsistencies to display.")
