2f989d182cdec09b78eb6a78da215f1e5950143b
InoriTsunemori <164060394+InoriTsunemori@users.noreply.github.com>
2024-08-16 13:20:08 +0200

    Update about_this.py

--- about_this.py
+++ about_this.py
@@ -19,36 +19,42 @@ def show_abt():
     c1,c2,c3=st.columns(3)
 
     with c1:
-        image_path = "images/nadine.png"
-        if os.path.isfile(image_path):
-            st.image(image_path, width=160)
+        image_path1 = "images/nadine.png"
+        if image_path1:
+            st.image(image_path1, width=180)
         else:
-            st.write(f"Archivo no encontrado en: {image_path}")
+            st.write(f"Archivo no encontrado")
+            
         st.write('Soy Nadine Poyato, una entusiasta de la salud (en concreto la salud infantil y la salud mental) que, tras desencantarse del trabajo en el campo de sanidad, se decidió por cambiar de sector.'
                  'Actualmente sigo ampliando mis conocimientos a la vez que realizo proyectos que aumentan cada día mi entusiasmo por el mundo del data science, y espero que siga en aumento.')
+        
         if st.link_button(':blue[Linkedin de Nadine]', "https://www.linkedin.com/in/paulacalviño"):
             st.balloons()
 
     with c2:
-        image_path = "images/paula.png"
-        if os.path.isfile(image_path):
-            st.image(image_path,  width=160)
+        image_path2 = "images/paula.png"
+        if image_path2:
+            st.image(image_path2,  width=180)
         else:
-            st.write(f"Archivo no encontrado en: {image_path}")
+            st.write(f"Archivo no encontrado")
+            
         st.write('Soy Paula Calviño. Anteriormente trabajaba en el sector de la logística pero actualmente me estoy reinventando y adentrando en el mundo del data science.'
                  'Estoy creciendo poco a poco en este nuevo sector y espero llegar a ser una profesional. '
                  'Queremos que este trabajo, en el que hemos puesto mucha ilusión y dedicación sirva para que la gente se conozca un poco más y para dar visibilidad a la importancia que tiene la salud mental.')
+        
         if st.link_button(':blue[Linkedin de Paula]', "https://www.linkedin.com/in/paulacalviño"):
             st.balloons()
 
     with c3:
-        image_path = "images/rosalia.png"
-        if os.path.isfile(image_path):
-            st.image(image_path,  width=160)
+        image_path3 = "images/rosalia.png"
+        if image_path3:
+            st.image(image_path3,  width=180)
         else:
-            st.write(f"Archivo no encontrado en: {image_path}")
+            st.write(f"Archivo no encontrado")
+            
         st.write('Soy Rosalía Reino, con experiencia en administración y gestión de bases de datos, actualmente en formación en data science. Mi interés en este campo me impulsa a expandir mis conocimientos y dedicarme plenamente a él. '
                  'Actualmente, estoy trabajando en varios proyectos para seguir ampliando mi formación y aplicar los conocimientos adquiridos. '
                  'En este proyecto, buscamos crear una herramienta para visibilizar y analizar la salud mental, contribuyendo a un tema de gran importancia en la sociedad.')
+        
         if st.link_button(':blue[LinkedIn de Rosalía]', "https://www.linkedin.com/in/rosaliareino"):
             st.balloons()
